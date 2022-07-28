f63d416d3bfa85ee4bb066585112ef54c74b0052
benluiwj <wenjie.lui@gmail.com>
2022-07-28 10:00:38 +0800

    Solve #27

--- Daily Coding Problem: Problem #027 [Easy].py
+++ Daily Coding Problem: Problem #027 [Easy].py
@@ -1,1 +1,36 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Facebook.# Given a string of round, curly, and square open and closing brackets, return# whether the brackets are balanced (well-formed).# For example, given the string "([])[]({})", you should return true.# Given the string "([)]" or "((()", you should return false.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Facebook.
+
+# Given a string of round, curly, and square open and closing brackets, return
+# whether the brackets are balanced (well-formed).
+
+# For example, given the string "([])[]({})", you should return true.
+
+# Given the string "([)]" or "((()", you should return false.
+
+def well_balanced(brackets):
+        stack = []
+        open_brackets = {'(', '[', '{'}
+        close_brackets = {')', ']', '}'}
+        for bracket in brackets:
+                if bracket in open_brackets:
+                        stack.append(bracket)
+                
+                else:
+                        if not stack:
+                                return False
+                        
+                        if bracket == ')' and stack[-1] == '(':
+                                stack.pop()
+                        
+                        elif bracket == ']' and stack[-1] == '[':
+                                stack.pop()
+                        
+                        elif bracket == '}' and stack[-1] == '{':
+                                stack.pop()
+                        
+                        else:
+                                return False
+        
+        return len(stack) == 0
