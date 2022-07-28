4d61edfe827cd3d8c5e3baeba738af8d74dc9525
benluiwj <wenjie.lui@gmail.com>
2022-07-28 09:53:52 +0800

    Update #25

--- Daily Coding Problem: Problem #025 [Hard].py
+++ Daily Coding Problem: Problem #025 [Hard].py
@@ -33,29 +33,49 @@ stop -> go till the end see if can
 if cannot, continue and try again the next time the next element match
 
 
+is_matched -> check whether text empty and whether the first letter of pattern is . or same as text
+
+dp method:
+
+initialise boolean array length of text
+
+
 
 """
-
-class Solution:
-        def isMatch(self, s: str, p: str) -> bool:
-                for i in range(len(p)):
-                        if p[i] == ".":
-                                continue
-                        
-                        elif p[i] == "*":
-                                prev = p[i-1]
-                                s_pter = i
-                                diff = len(p) - (i + 1)
-                                while s_pter != diff:
-                                        if prev != "." and prev != s[s_pter]:
-                                                return False
-                                        
-                                        s_pter += 1
-                        
-                        elif p[i] != s[i]:
-                                return False
+class Solution(object):
+        def isMatch(self, text, pattern):
+                if not pattern:
+                        return not text
                 
-                return True
+                # first_match = bool(text) and pattern[0] in {text[0], '.'}
+                
+                # if len(pattern) >= 2 and pattern[1] == '*':
+                #         return (self.isMatch(text, pattern[2:]) or
+                #                 first_match and self.isMatch(text[1:], pattern))
+                # else:
+                #         return first_match and self.isMatch(text[1:], pattern[1:])
+                
+                t = len(text)
+                p = len(pattern)
+                dp = [[False for _ in range(t + 1)] for _ in range(p + 1)]
+                
+                # last element is the empty string, hence they are both equal
+                dp[-1][-1] = True
+                
+                for i in range(t - 1, -1 , -1):
+                        for j in range(p - 1, -1 , -1):
+                                first_match = i < t and pattern[j] in {text[i], '.'}
+                                
+                                if j + 1 < p and p[j+1] == "*":
+                                        dp[i][j] = dp[i][j+2] or (first_match and dp[i+1][j])
+                                
+                                else:
+                                        dp[i][j] = first_match and dp[i+1][j+1]
+                
+                return dp[0][0]
+                
+                
+                        
                                         
                                         
                                         
