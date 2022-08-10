120521cce838cac6395461108fadd7f805da213a
benluiwj <wenjie.lui@gmail.com>
2022-08-10 17:02:35 +0800

    Solve 161

--- Daily Coding Problem: Problem #161 [Easy].py
+++ Daily Coding Problem: Problem #161 [Easy].py
@@ -1,1 +1,25 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Facebook.# Given a 32-bit integer, return the number with its bits reversed.# For example, given the binary number 1111 0000 1111 0000 1111 0000 1111 0000,# return 0000 1111 0000 1111 0000 1111 0000 1111.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Facebook.
+
+# Given a 32-bit integer, return the number with its bits reversed.
+
+# For example, given the binary number 1111 0000 1111 0000 1111 0000 1111 0000,
+# return 0000 1111 0000 1111 0000 1111 0000 1111.
+from typing import *
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        
+        for i in range(32):
+                most_sig = n & 1
+                result += most_sig * (2 ** (31 - i))
+                n = n >> 1
+        
+        print(result)
+        return result
+
+
+sol = Solution()
+sol.reverseBits(4294967293)
