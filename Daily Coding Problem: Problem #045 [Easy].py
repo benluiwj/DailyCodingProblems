2aefff7ba1bba80a31e7b7f1c17f41033bc59cda
benluiwj <wenjie.lui@gmail.com>
2022-08-12 13:59:59 +0800

    Solve 46

--- Daily Coding Problem: Problem #045 [Easy].py
+++ Daily Coding Problem: Problem #045 [Easy].py
@@ -1,1 +1,35 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Two Sigma.# Using a function rand5() that returns an integer from 1 to 5 (inclusive) with# uniform probability, implement a function rand7() that returns an integer from 1# to 7 (inclusive).
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Two Sigma.
+
+# Using a function rand5() that returns an integer from 1 to 5 (inclusive) with
+# uniform probability, implement a function rand7() that returns an integer from 1
+# to 7 (inclusive).
+
+import random
+
+
+"""
+
+we need to compute rand5() twice to cover the range of 7. 
+create the smallest 2d array such that there are equal numbers of 1-7
+fill the rest with 0
+
+now it would just be throwing darts until we dont get a 0
+
+"""
+
+def rand5():
+        return random.randint(1, 5)
+
+def rand7():
+        array = [[1,2,3,4,5], [6,7,1,2,3], [4,5,6,7,1], [2,3,4,5,6], [7,0,0,0,0]]
+        
+        while True:
+                i = rand5()
+                j = rand5()
+                
+                if array[i-1][j-1]:
+                        return array[i-1][j-1]
+
+print(rand7())
--- Daily Coding Problem: Problem #046 [Hard].py
+++ Daily Coding Problem: Problem #046 [Hard].py
@@ -1,1 +1,59 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Amazon.# Given a string, find the longest palindromic contiguous substring. If there are# more than one with the maximum length, return any one.# For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The# longest palindromic substring of "bananas" is "anana".
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Amazon.
+
+# Given a string, find the longest palindromic contiguous substring. If there are
+# more than one with the maximum length, return any one.
+
+# For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The
+# longest palindromic substring of "bananas" is "anana".
+
+"""
+
+take every contiguous substring and see if they are palindromes -> O(n^2) for every substring * O(n) for palindrome checking
+
+notice that if a substring is not a palindrome, adding to both ends wont make it one. have to add to either end.
+we can do bottom up dp in this case
+
+dp[i][j] would be a boolean value, indicating whether s[i:j] is a palindrome
+
+to check if s[i:j] is a palindrome, check if s[i+1:j-1] is a palindrome or not, provided s[i] == s[j]
+
+if s[i] != s[j], see if can form palindrome by taking checking if s[i] == s[j-1] or s[i+1] == s[j], then proceed with the normal palindrome checking.
+
+"""
+
+from typing import *
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        
+        dp = [[True for _ in range(n)] for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i] = True
+        
+        start = 0
+        end = 0
+        
+        for i in range(n - 2, -1 , -1):
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1]
+                
+                else:
+                    dp[i][j] = False
+                
+                if dp[i][j] and j - i + 1 > end - start + 1:
+                    start = i
+                    end = j
+                
+
+        
+        return s[start:end+1]
+        
+sol = Solution()
+print(sol.longestPalindrome("aabcdcb"))
+print(sol.longestPalindrome("bananas"))
+print(sol.longestPalindrome("cbbd"))
