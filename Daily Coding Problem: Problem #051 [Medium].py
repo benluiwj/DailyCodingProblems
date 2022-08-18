e3f9949fbc946153f9c0affdb0f44f49d8122d55
benluiwj <wenjie.lui@gmail.com>
2022-08-18 19:06:27 +0800

    Solve #51

--- Daily Coding Problem: Problem #051 [Medium].py
+++ Daily Coding Problem: Problem #051 [Medium].py
@@ -1,1 +1,27 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Facebook.# Given a function that generates perfectly random numbers between 1 and k# (inclusive), where k is an input, write a function that shuffles a deck of cards# represented as an array using only swaps.# It should run in O(N) time.# Hint: Make sure each one of the 52! permutations of the deck is equally likely.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Facebook.
+
+# Given a function that generates perfectly random numbers between 1 and k
+# (inclusive), where k is an input, write a function that shuffles a deck of cards
+# represented as an array using only swaps.
+
+# It should run in O(N) time.
+
+# Hint: Make sure each one of the 52! permutations of the deck is equally likely.
+
+import random
+
+"""
+
+Fisher-Yates shuffle
+
+Our loop invariant will be the following: at each index i of our loop, all indices before i have an equally random probability of being any element from our array.
+
+"""
+
+def shuffle_deck(cards):
+        n = len(cards)
+        for i in range(n-1):
+                j = random.randint(i, n-1)
+                cards[i], cards[j] = cards[j], cards[i]
