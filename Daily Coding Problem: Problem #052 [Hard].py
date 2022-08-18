135d2dcd86e5c458336286233ede3dd50ef750b6
benluiwj <wenjie.lui@gmail.com>
2022-08-18 21:01:22 +0800

    Implement linked list for #52

--- Daily Coding Problem: Problem #052 [Hard].py
+++ Daily Coding Problem: Problem #052 [Hard].py
@@ -22,31 +22,70 @@ use a doubly-linked list. create a dictionary that maps the value to the node it
 
 
 """
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        # dummy nodes
+        self.head = Node(None, 'head')
+        self.tail = Node(None, 'tail')
+        # set up head <-> tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get_head(self):
+        return self.head.next
+
+    def get_tail(self):
+        return self.tail.prev
+
+    def add(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
 class LRUCache:
+    def __init__(self, n):
+        self.n = n
+        self.dict = {}
+        self.list = LinkedList()
 
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = OrderedDict()
+    def put(self, key, val):
+        if key in self.dict:
+            self.dict[key].delete()
+        n = Node(key, val)
+        self.list.add(n)
+        self.dict[key] = n
+        if len(self.dict) > self.n:
+            head = self.list.get_head()
+            self.list.remove(head)
+            del self.dict[head.key]
 
-    def get(self, key: int) -> int:
-        if key in self.cache:
-            value = self.cache.pop(key)
-            self.cache[key] = value
-            return value
-        
-        return -1
+    def get(self, key):
+        if key in self.dict:
+            n = self.dict[key]
+            # bump to the back of the list by removing and adding the node
+            self.list.remove(n)
+            self.list.add(n)
+            return n.val
+            
+            
+            
+                
 
-    def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
-            self.cache[key] = value
-            
-            while len(self.cache) > self.capacity:
-                self.cache.popitem(False)
-            
-            
-        else:
-            self.cache.pop(key)
-            self.cache[key] = value
 
 cache1 = LRUCache(1)
 cache1.put(2,1)
