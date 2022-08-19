02c081a95c16bd4a15798a2ae652e1f19668ed0a
benluiwj <wenjie.lui@gmail.com>
2022-08-19 16:53:38 +0800

    Solve #55

--- Daily Coding Problem: Problem #055 [Easy].py
+++ Daily Coding Problem: Problem #055 [Easy].py
@@ -1,1 +1,48 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Microsoft.# Implement a URL shortener with the following methods:#  * shorten(url), which shortens the url into a six-character alphanumeric#    string, such as zLg6wl.#  * restore(short), which expands the shortened string into the original url. If#    no such shortened string exists, return null.# Hint: What if we enter the same URL twice?
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Microsoft.
+
+# Implement a URL shortener with the following methods:
+
+#  * shorten(url), which shortens the url into a six-character alphanumeric
+#    string, such as zLg6wl.
+#  * restore(short), which expands the shortened string into the original url. If
+#    no such shortened string exists, return null.
+
+# Hint: What if we enter the same URL twice?
+
+from typing import *
+import string
+import random
+
+class Codec:
+    def __init__(self):
+        self.url_to_short = dict()
+        self.short_to_url = dict()
+        self.alphanumerals = string.ascii_lowercase() + string.ascii_uppercase() + string.digits()
+        self.length = len(self.alphanumerals)
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        if longUrl in self.url_to_short:
+            return  self.url_to_short[longUrl]
+        
+        shortUrl = ""
+        for i in range(6):
+            shortUrl += self.alphanumerals[random.randint(0, self.length - 1)]
+        
+        self.url_to_short[longUrl] = shortUrl
+        self.short_to_url[shortUrl] = longUrl
+        
+        return shortUrl
+        
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        if shortUrl not in self.short_to_url:
+            return None
+        
+        return self.short_to_url[shortUrl]
