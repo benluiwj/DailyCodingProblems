cc8d3b60e62c55618f8b7a9f99aba3f529e7cbf8
benluiwj <wenjie.lui@gmail.com>
2022-08-21 20:16:33 +0800

    Solve #62

--- Daily Coding Problem: Problem #061 [Medium].py
+++ Daily Coding Problem: Problem #061 [Medium].py
@@ -1,1 +1,42 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Google.# Implement integer exponentiation. That is, implement the pow(x, y) function,# where x and y are integers and returns x^y.# Do this faster than the naive method of repeated multiplication.# For example, pow(2, 10) should return 1024.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Google.
+
+# Implement integer exponentiation. That is, implement the pow(x, y) function,
+# where x and y are integers and returns x^y.
+
+# Do this faster than the naive method of repeated multiplication.
+
+# For example, pow(2, 10) should return 1024.
+
+"""
+
+naive method runs in O(n) time where n is the exponent
+
+split exponent into half instead. Now it will run in O(log n)
+
+"""
+
+
+from typing import *
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        result = x
+        exponent = n
+        if n < 0:
+            result = 1/x
+            exponent = -n
+        
+        coeff = 1
+        while exponent > 1:
+            if exponent % 2 == 0:
+                exponent = exponent // 2
+                result *= result
+            
+            else:
+                coeff *= result
+                result *= result
+                exponent = (exponent - 1) // 2
+        
+        return coeff * result
--- Daily Coding Problem: Problem #062 [Medium].py
+++ Daily Coding Problem: Problem #062 [Medium].py
@@ -1,1 +1,33 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Facebook.# There is an N by M matrix of zeroes. Given N and M, write a function to count# the number of ways of starting at the top-left corner and getting to the# bottom-right corner. You can only move right or down.# For example, given a 2 by 2 matrix, you should return 2, since there are two# ways to get to the bottom-right:#  * Right, then down#  * Down, then right# Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Facebook.
+
+# There is an N by M matrix of zeroes. Given N and M, write a function to count
+# the number of ways of starting at the top-left corner and getting to the
+# bottom-right corner. You can only move right or down.
+
+# For example, given a 2 by 2 matrix, you should return 2, since there are two
+# ways to get to the bottom-right:
+
+#  * Right, then down
+#  * Down, then right
+
+# Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+
+from typing import *
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        for i in range(n):
+            dp[0][i] = 1
+        
+        for i in range(m):
+            dp[i][0] = 1
+        
+        for i in range(1 , m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[-1][-1]
