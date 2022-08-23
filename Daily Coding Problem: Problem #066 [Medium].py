66153c17180b8f33fe4dc8d0d00c327406560520
benluiwj <wenjie.lui@gmail.com>
2022-08-23 15:39:50 +0800

    Solve #66

--- Daily Coding Problem: Problem #066 [Medium].py
+++ Daily Coding Problem: Problem #066 [Medium].py
@@ -1,1 +1,34 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Square.# Assume you have access to a function toss_biased() which returns 0 or 1 with a# probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the# bias of the coin.# Write a function to simulate an unbiased coin toss.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Square.
+
+# Assume you have access to a function toss_biased() which returns 0 or 1 with a
+# probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the
+# bias of the coin.
+
+# Write a function to simulate an unbiased coin toss.
+
+
+"""
+
+Let's draw out the probability chart for tossing our coin twice. Let's say the probability of getting heads is p, so tails is 1 - p:
+
+HH: p * p
+HT: p * (1 - p)
+TH: (1 - p) * p
+TT: (1 - p) * (1 - p)
+Since multiplication is commutative, we find that flipping heads and then tails has the same probability of flipping tails, then heads!
+
+
+toss coin twice
+
+if heads,tails -> return heads
+if tails, heads -> return tails
+
+"""
+
+def toss_bias():
+        pass
+
+def unbiased_toss():
+        pass
