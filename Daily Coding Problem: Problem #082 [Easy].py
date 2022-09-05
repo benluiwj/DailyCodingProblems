3472d18651f83e29e4b24ce68f93408c345e9b59
benluiwj <wenjie.lui@gmail.com>
2022-09-05 20:38:31 +0800

    Solve #82

--- Daily Coding Problem: Problem #082 [Easy].py
+++ Daily Coding Problem: Problem #082 [Easy].py
@@ -1,1 +1,24 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked Microsoft.# Using a read7() method that returns 7 characters from a file, implement readN(n)# which reads n characters.# For example, given a file with the content “Hello world”, three read7() returns# “Hello w”, “orld” and then “”.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked Microsoft.
+
+# Using a read7() method that returns 7 characters from a file, implement readN(n)
+# which reads n characters.
+
+# For example, given a file with the content “Hello world”, three read7() returns
+# “Hello w”, “orld” and then “”.
+
+def read7():
+        pass
+
+def readN(n):
+        if n % 7 == 0:
+                product = n // 7
+                for _ in range(product):
+                        read7()
+                return
+        
+        product = n // 7
+        for _ in range(product + 1):
+                read7()
+        
