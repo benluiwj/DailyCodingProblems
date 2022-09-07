90b00aa9be5ed59261a512d711fdd63527b12064
benluiwj <wenjie.lui@gmail.com>
2022-09-07 20:40:07 +0800

    Solve #90

--- Daily Coding Problem: Problem #090 [Medium].py
+++ Daily Coding Problem: Problem #090 [Medium].py
@@ -1,1 +1,21 @@
-# Good morning! Here's your coding interview problem for today.# This question was asked by Google.# Given an integer n and a list of integers l, write a function that randomly# generates a number from 0 to n-1 that isn't in l (uniform).
+# Good morning! Here's your coding interview problem for today.
+
+# This question was asked by Google.
+
+# Given an integer n and a list of integers l, write a function that randomly
+# generates a number from 0 to n-1 that isn't in l (uniform).
+
+import random
+
+"""
+generate numbers till we find one that isnt in l. Possible infinite worsetime
+
+just do it naively; create a list from 0 to n-1 that subtract elements that are in l...
+
+"""
+
+def uniform_generator(n, l):
+        while True:
+                generated_num = random.randint(0, n-1)
+                if generated_num not in l:
+                        return generated_num
