84f7d66f8254fa150fc85f38c030bc59c3cafd65
benluiwj <wenjie.lui@gmail.com>
2022-09-22 19:46:23 +0800

    solve #99

--- Problems 1 - 100/Daily Coding Problem: Problem #099 [Medium].py
+++ Problems 1 - 100/Daily Coding Problem: Problem #099 [Medium].py
@@ -1,1 +1,27 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Microsoft.# Given an unsorted array of integers, find the length of the longest consecutive# elements sequence.# For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element# sequence is [1, 2, 3, 4]. Return its length: 4.# Your algorithm should run in O(n) complexity.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Microsoft.
+
+# Given an unsorted array of integers, find the length of the longest consecutive
+# elements sequence.
+
+# For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element
+# sequence is [1, 2, 3, 4]. Return its length: 4.
+
+# Your algorithm should run in O(n) complexity.
+
+def consecutive_elements(nums):
+        set_nums = set(nums)
+        
+        result = 0
+        for num in nums:
+                if num - 1 not in set_nums:
+                        length = 1
+                        temp = num
+                        while temp in set_nums:
+                                temp += 1
+                                length += 1
+                        
+                        result = max(result, length)
+        
+        return result
