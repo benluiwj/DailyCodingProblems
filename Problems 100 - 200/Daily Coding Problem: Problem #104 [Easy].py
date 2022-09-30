6bd2ea762875a6477e68234d35888761b94e141f
benluiwj <wenjie.lui@gmail.com>
2022-09-30 17:04:28 +0800

    solve #104

--- Problems 100 - 200/Daily Coding Problem: Problem #104 [Easy].py
+++ Problems 100 - 200/Daily Coding Problem: Problem #104 [Easy].py
@@ -1,1 +1,36 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Google.# Determine whether a doubly linked list is a palindrome. What if it’s singly# linked?# For example, 1 -> 4 -> 3 -> 4 -> 1 returns True while 1 -> 4 returns False.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Google.
+
+# Determine whether a doubly linked list is a palindrome. What if it’s singly
+# linked?
+
+# For example, 1 -> 4 -> 3 -> 4 -> 1 returns True while 1 -> 4 returns False.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import *
+
+"""
+
+reverse the linked list then iterate through to see if they are the same or not. once pass the middle can return already.
+
+"""
+
+class Solution:
+        def isPalindrome(self, head: Optional[ListNode]) -> bool:
+                if not head or not head.next:
+                        return True
+                
+                lst = []
+                
+                temp = head
+                
+                while temp:
+                        lst.append(temp.val)
+                        temp = temp.next
+                        
+                return lst == lst[::-1]
