f93f16ccc6aabcb9fab3619f401fb239f6551fca
benluiwj <wenjie.lui@gmail.com>
2022-10-01 12:50:27 +0800

    solve #107

--- Problems 100 - 200/Daily Coding Problem: Problem #107 [Easy].py
+++ Problems 100 - 200/Daily Coding Problem: Problem #107 [Easy].py
@@ -1,1 +1,31 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Microsoft.# Print the nodes in a binary tree level-wise. For example, the following should# print 1, 2, 3, 4, 5.#   1#  / \# 2   3#    / \#   4   5
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Microsoft.
+
+# Print the nodes in a binary tree level-wise. For example, the following should
+# print 1, 2, 3, 4, 5.
+
+#   1
+#  / \
+# 2   3
+#    / \
+#   4   5
+
+def level_traversal(root):
+        if not root:
+                return
+        
+        frontier = [root]
+        
+        while frontier:
+                new_frontier = []
+                for node in frontier:
+                        print(node.val)
+                        if node.left:
+                                new_frontier.append(node.left)
+                        
+                        if node.right:
+                                new_frontier.append(node.right)
+                
+                frontier = new_frontier
+                
