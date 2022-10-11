bb6dcfc9e7bb34db675653a573f6f4b59f6f76b3
benluiwj <wenjie.lui@gmail.com>
2022-10-11 13:20:42 +0800

    solve #117

--- Problems 100 - 200/Daily Coding Problem: Problem #117 [Easy].py
+++ Problems 100 - 200/Daily Coding Problem: Problem #117 [Easy].py
@@ -1,1 +1,28 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Facebook.# Given a binary tree, return the level of the tree with minimum sum.
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Facebook.
+
+# Given a binary tree, return the level of the tree with minimum sum.
+def least_sum(root):
+        if not root:
+                return 0
+        
+        lowest = root.val
+        frontier = [root]
+        
+        while frontier:
+                new_frontier = []
+                total = 0
+                for node in frontier:
+                        if node.left:
+                                total += node.left.val
+                                new_frontier.append(node.left)       
+                        
+                        if node.right:
+                                total += node.right.val
+                                new_frontier.append(node.right)
+                
+                lowest = min(lowest, total)
+                frontier = new_frontier
+        
+        return lowest
