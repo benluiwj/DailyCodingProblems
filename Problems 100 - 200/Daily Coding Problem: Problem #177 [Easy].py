7e7fa919abc4634de48bbfdae696b82ee05b2cb5
benluiwj <wenjie.lui@gmail.com>
2023-01-03 16:49:56 +0800

    solve 177

--- Problems 100 - 200/Daily Coding Problem: Problem #177 [Easy].py
+++ Problems 100 - 200/Daily Coding Problem: Problem #177 [Easy].py
@@ -1,1 +1,52 @@
-# Good morning! Here's your coding interview problem for today.# This problem was asked by Airbnb.# Given a linked list and a positive integer k, rotate the list to the right by k # places.# For example, given the linked list 7 -> 7 -> 3 -> 5 and k = 2, it should become # 3 -> 5 -> 7 -> 7.# Given the linked list 1 -> 2 -> 3 -> 4 -> 5 and k = 3, it should become 3 -> 4# -> 5 -> 1 -> 2.from typing import * 
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Airbnb.
+
+# Given a linked list and a positive integer k, rotate the list to the right by k 
+# places.
+
+# For example, given the linked list 7 -> 7 -> 3 -> 5 and k = 2, it should become 
+# 3 -> 5 -> 7 -> 7.
+
+# Given the linked list 1 -> 2 -> 3 -> 4 -> 5 and k = 3, it should become 3 -> 4
+# -> 5 -> 1 -> 2.
+from typing import * 
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def rotateLL(head):
+    prev = None
+    curr = head
+    while curr.next:
+        prev = curr
+        curr = curr.next
+    
+    prev.next = None
+    curr.next = head
+    return curr
+    
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        length = 0
+        temp = head
+        while temp:
+            length += 1
+            temp = temp.next
+        
+        rotate = k % length
+        new_head = head
+        while rotate:
+            new_head = rotateLL(new_head)
+            rotate -= 1
+        
+        return new_head
+
+        
